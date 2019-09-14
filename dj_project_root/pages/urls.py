a8a728d6d994885637c73d34319c09e3709a1102
jisshub <jissmon476@gmial.com>
2019-09-14 09:34:21 +0530

    added linking and now using if

--- dj_project_root/pages/urls.py
+++ dj_project_root/pages/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('about/', views.about, name='about'),
 ]
