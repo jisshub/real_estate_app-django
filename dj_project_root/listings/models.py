ea381899089b4063e074ad7e8f8a72fb2c10ef74
jisshub <jissmon476@gmial.com>
2019-09-17 08:41:15 +0530

    created listings models

--- dj_project_root/listings/models.py
+++ dj_project_root/listings/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+class Listing(models.Model):
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=30)
+    zipcode = models.CharField(max_length=20)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
+    garage = models.IntegerField(default=0)
+    sqft = models.IntegerField()
+    lotsize = models.FloatField()
+    ispublished = models.BooleanField(default=True)
+    listdate = models.DateField()
+    photomain = models.ImageField(upload_to='photos/%Y/%m/%d/')
+
+    def __str__(self):
+        return self.title
--- dj_project_root/realtors/models.py
+++ dj_project_root/realtors/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Realtor(models.Model):
+    name = models.CharField(max_length=30)
+    photo = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    email = models.CharField(max_length=30)
+    phone = models.CharField(max_length=20)
+    ismvp = models.BooleanField(default=False)
+    hireDate = models.DateField()
+
+    def __str__(self):
+        return self.name
