0e18a6148c400e3d616828459c60ccc08356bd7f
jisshub <jissmon476@gmial.com>
2019-09-22 00:21:36 +0530

    displayed listings in template

--- dj_project_root/listings/urls.py
+++ dj_project_root/listings/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('', views.index, name='listings'),
     # for each single listings. v need to include an id for each listing..
     # eg, listings/23. for that v use a parameter called listing_id,
-    path('<int: listing_id>', views.listing, name='listing'),
+    path('<int:listing_id>', views.listing, name='listing'),
     # here int is the type of parameter. <int: listing_id> is the way to access the id from the view
-    # method.
+    # method. it point to listing view in views.py, so v need to specify this listing_id parameter there also.
     path('search/', views.search, name='search'),
 
 ]
--- dj_project_root/listings/views.py
+++ dj_project_root/listings/views.py
@@ -6,12 +6,18 @@ from .models import Listing
 
 # Create your views here.
 def index(request):
-    each_listing = Listing.objects.all()
-    list_dict = {'list': each_listing}
-    return render(request, 'listings/listings.html', list_dict)
+    # fetch all objects
+    listings = Listing.objects.all()
+    # pass it to a dictionary
+    context = {
+        'listing': listings
+    }
+    # render html and pass the dictionary as argument
+    return render(request, 'listings/listings.html', context)
 
 
-def listing(request):
+def listing(request, listing_id):
+    # listing_id parameter given in listing url in urls.py file
     return render(request, 'listings/listing.html')
 
 
