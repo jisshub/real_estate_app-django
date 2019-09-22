deec14ae7c0f709d36f28cb26b47ac051f365b13
jisshub <jissmon476@gmial.com>
2019-09-22 13:57:44 +0530

    completed paginations in listings template

--- dj_project_root/listings/views.py
+++ dj_project_root/listings/views.py
@@ -1,6 +1,6 @@
 from django.http import request
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
 from .models import Listing
 
 
@@ -8,9 +8,15 @@ from .models import Listing
 def index(request):
     # fetch all objects
     listings = Listing.objects.all()
+    # create a paginator and pass the listings set and an argument
+    # 3 means listings per page
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    page_listing = paginator.get_page(page)
+
     # pass it to a dictionary
     context = {
-        'listing': listings
+        'listing': page_listing
     }
     # render html and pass the dictionary as argument
     return render(request, 'listings/listings.html', context)
@@ -18,6 +24,7 @@ def index(request):
 
 def listing(request, listing_id):
     # listing_id parameter given in listing url in urls.py file
+
     return render(request, 'listings/listing.html')
 
 
